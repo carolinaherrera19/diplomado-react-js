fc211e91549b47bb67109cb2be34a22452af9402
carolinaherrera19 <lifaherrera@gmail.com>
2022-10-29 02:29:46 -0500

    ciclos

---
+++ ejemplos clase/polinomio/polinomio.js
@@ -0,0 +1,11 @@
+function polimonio(x) {
+    y = math.pow(x, 2)    
+    respuesta = (y + x + 1)
+    
+    return respuesta
+
+    x = parseInt(window.prompt("ingresa el numero"))
+
+    console.log(" es : " + respuesta(x))
+}
+polimonio()
