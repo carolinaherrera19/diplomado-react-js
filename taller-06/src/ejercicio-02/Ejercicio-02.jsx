ab4fd9eab20055aa0dded0df23ffbc8f1a41995f
carolinaherrera19 <lifaherrera@gmail.com>
2022-11-22 18:18:18 -0500

    finalizacion ejercicio 2

--- taller-06/src/ejercicio-02/Ejercicio-02.jsx
+++ taller-06/src/ejercicio-02/Ejercicio-02.jsx
@@ -87,21 +87,13 @@ class Ejercicio02 extends React.Component {
             <Card.Img variant="top" src={receta.imagen} />
             <Card.Body>
               <Card.Title>{receta.nombre}</Card.Title>
-              <h5>
-                <Card.Text>Descripcion:</Card.Text>
-              </h5>
+              <h5>Descripcion:</h5>
               <Card.Text>{receta.descripcion}</Card.Text>
-              <h5>
-                <Card.Text>Ingredientes:</Card.Text>
-              </h5>
-              <Card.Text>
-                <ul>
-                  <ListaIngredientes ingredientes={receta.ingredientes} />
-                </ul>
-              </Card.Text>
-              <h5>
-                <Card.Text>Preparacion:</Card.Text>
-              </h5>
+              <h5>Ingredientes:</h5>
+              <ul>
+                <ListaIngredientes ingredientes={receta.ingredientes} />
+              </ul>
+              <h5>Preparacion:</h5>
               <Card.Text>{receta.preparacion}</Card.Text>
             </Card.Body>
           </Card>
