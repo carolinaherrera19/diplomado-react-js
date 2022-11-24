4e04264ab2a8c7b19c189edd57e78af986adbae7
carolinaherrera19 <lifaherrera@gmail.com>
2022-11-23 20:12:05 -0500

    agregando scroll en la letra

--- taller-06/src/componentes/cancion-detalle.jsx
+++ taller-06/src/componentes/cancion-detalle.jsx
@@ -7,24 +7,17 @@ function createMarkup(html) {
 }
 
 function CancionDetalle() {
-  let { cancionId } = useParams();
+  const { cancionId } = useParams();
   const cancion = CancionesJSON.find((c) => c.id === +cancionId);
   return (
     <Col className="text-center" md={{ span: 6, offset: 3 }}>
       <h1 className="text-capitalize">{cancion.nombreCancion}</h1>
-      <div contentEditable="true" dangerouslySetInnerHTML={createMarkup(cancion.linkVideo)} />
+      <div dangerouslySetInnerHTML={createMarkup(cancion.linkVideo)} />
 
       <ul className="lista-letra">
-        {cancion.letra
-          .map((l, index) => {
-            let items = [];
-            if (index % 4 === 0) {
-              items.push(<li key={`blank-${index}`}>-</li>);
-            }
-            items.push(<li key={index}>{l}</li>);
-            return items;
-          })
-          .flat()}
+        {cancion.letra.map((l, index) => (
+          <li key={index}>{l}</li>
+        ))}
       </ul>
     </Col>
   );
--- taller-06/src/pages/recetas/recetas.jsx
+++ taller-06/src/pages/recetas/recetas.jsx
@@ -9,26 +9,24 @@ function ListaIngredientes(props) {
 
 class Recetas extends React.Component {
   render() {
-    return RecetasJSON.map((receta) => {
-      return (
-        <Col key={receta.id}>
-          <Card>
-            <Card.Img variant="top" src={receta.imagen} />
-            <Card.Body>
-              <Card.Title>{receta.nombre}</Card.Title>
-              <h5>Descripcion:</h5>
-              <Card.Text>{receta.descripcion}</Card.Text>
-              <h5>Ingredientes:</h5>
-              <ul>
-                <ListaIngredientes ingredientes={receta.ingredientes} />
-              </ul>
-              <h5>Preparacion:</h5>
-              <Card.Text>{receta.preparacion}</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      );
-    });
+    return RecetasJSON.map((receta) => (
+      <Col key={receta.id}>
+        <Card>
+          <Card.Img variant="top" src={receta.imagen} />
+          <Card.Body>
+            <Card.Title>{receta.nombre}</Card.Title>
+            <h5>Descripcion:</h5>
+            <Card.Text>{receta.descripcion}</Card.Text>
+            <h5>Ingredientes:</h5>
+            <ul>
+              <ListaIngredientes ingredientes={receta.ingredientes} />
+            </ul>
+            <h5>Preparacion:</h5>
+            <Card.Text>{receta.preparacion}</Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+    ));
   }
 }
 
